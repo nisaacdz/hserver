668dd307c28c063213464510d079e0519a0ccb14
Isaac <nisaacdz@gmail.com>
2025-11-25 13:45:36 +0000

    Create api dtos module

---
+++ api/src/dtos/mod.rs
@@ -0,0 +1,15 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct LoginRequest {
+    pub email: String,
+    pub password: String,
+}
+
+#[derive(Deserialize)]
+pub struct FindRoom {
+    pub class_id: String,
+    pub period: (i32, i32),
+}
+
+// Add chrono, uuid, crypto or
