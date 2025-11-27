0acf22048c412eac320842e068dd4222c6780d51
Isaac <nisaacdz@gmail.com>
2025-11-27 22:37:51 +0000

    Fix blocks use TSTZRANGE for intervals

--- migrations/2025-11-24-203546-0000_blocks/up.sql
+++ migrations/2025-11-24-203546-0000_blocks/up.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION IF NOT EXISTS btree_gist;
 CREATE TABLE blocks (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     room_id UUID NOT NULL REFERENCES rooms(id),
-    interval TSRANGE NOT NULL,
+    interval TSTZRANGE NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     CONSTRAINT no_overlaps EXCLUDE USING GIST (
