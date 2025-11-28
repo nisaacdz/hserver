00cf7b402ab8d78e3c0ed777027966b1f142da16
Isaac <nisaacdz@gmail.com>
2025-11-28 02:19:45 +0000

    Removed role on user table

--- migrations/2025-11-24-144011-0000_users/up.sql
+++ migrations/2025-11-24-144011-0000_users/up.sql
@@ -4,7 +4,6 @@ CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     email TEXT NOT NULL UNIQUE,
     password_hash TEXT,
-    role TEXT NOT NULL DEFAULT 'customer',
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
