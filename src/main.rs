4fb510984f5c751d8ba9fc90426f46b26529458f
Isaac <nisaacdz@gmail.com>
2025-12-18 15:43:06 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ use tracing::info;
 async fn main() -> std::io::Result<()> {
     dotenvy::dotenv().ok();
     tracing_subscriber::fmt::init();
+    
+    let run_mode = std::env::var("RUN_MODE").ok().filter(|m| ["production", "development", "test"].contains(&m.as_str())).unwrap_or("development".to_string());
 
     let settings = {
-        let run_mode = std::env::var("RUN_MODE").unwrap_or("development".to_string());
 
         let settings: app::AppSettings = config::Config::builder()
             .add_source(config::File::with_name("config/default"))
@@ -20,11 +21,14 @@ async fn main() -> std::io::Result<()> {
         settings
     };
 
-    let pool = infra::db::init_pool(&settings.database).expect("Failed to initialize pg connection pool");
+    let pool =
+        infra::db::init_pool(&settings.database).expect("Failed to initialize pg connection pool");
 
-    info!("Starting server at http://localhost:8080");
+    info!("Starting server at http://localhost:8080 in {run_mode}");
 
-    migrator::run_migrations(&pool).await.expect("Failed to run migrations");
+    migrator::run_migrations(&pool)
+        .await
+        .expect("Failed to run migrations");
 
     api::run(pool, settings).await
 }
